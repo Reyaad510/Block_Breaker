e6faffcfa29bb97dc7865afe3155d1405888121a
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-01 13:05:34 -0500

    When ball hits block, the block will be destroyed. Made block bigger for testing

---
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // when something collides that collision moment will tell us  what was the colision
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Destroy(obj, float t = 0.0f meaning how long you want it to get destroyed)
+        // gameObject not captial referring to "this" gameObecjt(being the block)
+        Destroy(gameObject);
+
+        // tells us what collided with us. In this case would be "Ball"
+        Debug.Log(collision.gameObject.name);
+    }
+}
