6f72b6751cdc66b3895bec19566e5504accf0f23
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-11-30 23:23:29 -0500

    Created barrier so paddle doesn't fly off screen using Mathf.Clamp()

--- Block Breaker/Assets/Scripts/Paddle.cs
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour
-{   
+
+    // configuration parameters
+{
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15;
     [SerializeField] float screenWidthInUnits = 16f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +24,10 @@ public class Paddle : MonoBehaviour
         float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
 
         // compact way of storing x and y positions
-        Vector2 paddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+
+        // Mathf.Clamp you pass in the float number and give it a min number and max number this being the end of the screens so the paddle wont fly off
+        paddlePosition.x = Mathf.Clamp(mousePositionInUnits, minX, maxX);
         transform.position = paddlePosition;
         
     }
