d79d1e1c22c5a0b419f09ff08d36797325291e05
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-01 01:47:51 -0500

    User able to left click to launch ball from paddle

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -8,9 +8,12 @@ public class Ball : MonoBehaviour
     // config parameters
 
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
 
     // state
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +25,26 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // get position of paddle
+        // bool used bcuz if not then when you left click the ball wont launch off paddle
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePosition + paddleToBallVector;
     }
