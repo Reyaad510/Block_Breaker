88c3d894b816dce42b4ab6e2adb56651c4511518
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-11-30 22:52:25 -0500

    Created trigger collision when ball goes past paddle, which trigger game over screen

---
+++ Block Breaker/Assets/Sprites/LoseCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    // This is to say when we have the ball collider go through the box trigger collider then to end game
+    // Normally wouldnt use hard coded string "Game Over"(referring to scene in build settings) but since we know we will never change it is okay. 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+}
