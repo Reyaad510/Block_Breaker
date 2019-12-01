c7e89efa617fa06b260d95e25f5bb1329b71ea62
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-01 01:26:06 -0500

    Ball now sticks to paddle following with mouse position using Vector2 manipulation

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    // config parameters
+
+    [SerializeField] Paddle paddle1;
+
+    // state
+    Vector2 paddleToBallVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // giving distance between ball and paddle. Postion of ball minus position of paddle x1,y1 - x2,y2
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get position of paddle
+        Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePosition + paddleToBallVector;
+    }
+}
