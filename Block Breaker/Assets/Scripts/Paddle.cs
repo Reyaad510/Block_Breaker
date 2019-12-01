45a454064ec7d97745c8a0ec83fe827765983961
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-11-30 23:17:06 -0500

    Hooked up paddle to be where mouse position is. Programmed so doesnt matter what scren size is.

---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{   
+    [SerializeField] float screenWidthInUnits = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         // get mouse position according to pixels and length in our case being 16 bcuz 4:3
+        float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+
+        // compact way of storing x and y positions
+        Vector2 paddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
+        transform.position = paddlePosition;
+        
+    }
+}
