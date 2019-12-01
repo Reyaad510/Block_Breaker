930cab6ec84ac735701f892af2501ea45185032c
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-01 16:48:15 -0500

    When a game over occurs the score now resets back to zero

--- Block Breaker/Assets/Scripts/GameStatus.cs
+++ Block Breaker/Assets/Scripts/GameStatus.cs
@@ -14,6 +14,26 @@ public class GameStatus : MonoBehaviour
     [SerializeField] int currentScore = 0;
 
 
+    // Awake occurs before Start
+    // With below put Game Canvas in Game Status to carry over the score and delete the GameCanvas from other levels
+    private void Awake()
+    {   // FindObjects(plural)
+        // Looking for all GameStatus we have. If there is one then destroy yourself, if not then dont destroy when the level loads
+        int gameStatusCont = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCont > 1)
+        {
+            // Fixes a bug with Singleton 
+            gameObject.SetActive(false);
+            // Destroy yourself
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
     private void Start()
     {
         scoreText.text = currentScore.ToString();
@@ -24,11 +44,16 @@ public class GameStatus : MonoBehaviour
         Time.timeScale = gameSpeed;
     }
 
-    public void addToScore()
+    public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
         scoreText.text = currentScore.ToString();
 
 
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
     }
 }
