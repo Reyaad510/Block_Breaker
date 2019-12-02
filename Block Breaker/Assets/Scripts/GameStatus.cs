f230e7a6efe270c699debe2a4b329e5b92f67f9b
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2019-12-02 13:17:17 -0500

    Added 3 more levels. Need to fix ball loop that occurs

--- Block Breaker/Assets/Scripts/GameStatus.cs
+++ Block Breaker/Assets/Scripts/GameStatus.cs
@@ -9,6 +9,7 @@ public class GameStatus : MonoBehaviour
     [Range(0.5f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoPlayEnabled;
 
     // state variables
     [SerializeField] int currentScore = 0;
@@ -55,5 +56,10 @@ public class GameStatus : MonoBehaviour
     public void ResetGame()
     {
         Destroy(gameObject);
+    }
+
+    public bool IsAutoPlayEnabled()
+    {
+        return isAutoPlayEnabled;
     }
 }
--- Block Breaker/Assets/Scripts/Paddle.cs
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -10,25 +10,39 @@ public class Paddle : MonoBehaviour
     [SerializeField] float maxX = 15;
     [SerializeField] float screenWidthInUnits = 16f;
 
+    // cached references
+    GameStatus theGameStatus;
+    Ball theBall;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        theGameStatus = FindObjectOfType<GameStatus>();
+        theBall = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
-         // get mouse position according to pixels and length in our case being 16 bcuz 4:3
-        float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-
         // compact way of storing x and y positions
         Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
 
         // Mathf.Clamp you pass in the float number and give it a min number and max number this being the end of the screens so the paddle wont fly off
-        paddlePosition.x = Mathf.Clamp(mousePositionInUnits, minX, maxX);
+        paddlePosition.x = Mathf.Clamp(GetXPos(), minX, maxX);
         transform.position = paddlePosition;
         
+    }
+
+    private float GetXPos()
+    {
+        if (theGameStatus.IsAutoPlayEnabled())
+        {
+            // Used to enable autoplay for testing for developers
+            return theBall.transform.position.x;
+        }
+        else
+        { // get mouse position according to pixels and length in our case being 16 bcuz 4:3
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
     }
 }
